e61f018d6703b8d9d0b8f99999d7c04bf07fcf2f
Camila <camidv14@gmail.com>
2018-04-14 13:16:12 -0500

    actividad aircombat

---
+++ MariaCamilaDaza-AirCombat/Assets/Scripts/Missile2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.Translate (0.0f, 2.0f, 0.0f);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+
+		if (collision.collider.gameObject.tag == "Enemy") {
+
+			GameObject explosion = GameObject.Instantiate(Resources.Load ("Prefabs/Explosion") as GameObject);
+
+			explosion.transform.position = collision.collider.gameObject.transform.position; 
+
+			Destroy (collision.collider.gameObject); 
+
+			Destroy (this.gameObject);
+
+		}
+
+		if (collision.collider.gameObject.tag == "Ally") {
+
+			Destroy (this.gameObject);
+
+		}
+
+		if (collision.collider.gameObject.tag == "Terreno") {
+
+			Destroy (this.gameObject);
+
+		}
+	
+	}
+}
